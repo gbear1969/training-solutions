15402277de0ca34728b6147239260e15a1656d82
gbear1969 <geber.andras@freemail.hu>
2020-11-22 15:44:05 +0100

    introcontrol

--- src/main/java/introcontrol/IntroControl.java
+++ src/main/java/introcontrol/IntroControl.java
@@ -7,4 +7,69 @@ public class IntroControl {
         }
         return number;
     }
+
+    public String describeNumber(int number) {
+        String nulla;
+        if (number == 0) {
+            nulla = "zero";
+        } else {
+            nulla = "not zero";
+        }
+        return nulla;
+    }
+
+    public String greatingToJoe(String name) {
+        String hello = "";
+        if ("Joe".equals(name)) {
+            hello = "Hello Joe";
+        }
+        return hello;
+    }
+
+    public int calculateBonus(int sale) {
+        int bonus = (int) (sale * 0.1 + 0.5);
+        if (sale < 1_000_000) {
+            bonus = 0;
+        }
+        return bonus;
+    }
+
+    public int calculateConsumption(int prev, int next) {
+        if (next<prev) {
+            next = next + 10000;
+        }
+        return  next-prev;
+    }
+
+    public void printNumbers(int max) {
+        for (int i = 0; i <= max; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public void printNumbersBetween(int min, int max) {
+        for (int i = min; i <= max; i++) {
+            System.out.println(i);
+        }
+    }
+
+    void printNumbersBetweenAnyDirection(int a, int b) {
+        if (a > b) {
+            for (int i = a; i >= b; i--) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = a; i <= b; i++) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    public void printOddNumbers(int max) {
+        for (int i = 1; i <= max; i = i + 2) {
+            System.out.println(i);
+        }
+    }
 }
+
--- src/main/java/introcontrol/IntroControlMain.java
+++ src/main/java/introcontrol/IntroControlMain.java
@@ -2,8 +2,32 @@ package introcontrol;
 
 public class IntroControlMain {
     public static void main(String[] args) {
-        IntroControl szam = new IntroControl();
-        System.out.println(szam.subtractTenIfGreaterThanTen(10));
-        System.out.println(szam.subtractTenIfGreaterThanTen(11));
+        IntroControl kontroll = new IntroControl();
+        System.out.println(kontroll.subtractTenIfGreaterThanTen(10));
+        System.out.println(kontroll.subtractTenIfGreaterThanTen(11));
+        System.out.println();
+        System.out.println(kontroll.describeNumber(0));
+        System.out.println(kontroll.describeNumber(2));
+        System.out.println();
+        System.out.println(kontroll.greatingToJoe("Joe"));
+        System.out.println(kontroll.greatingToJoe("Joan"));
+        System.out.println();
+        System.out.println(kontroll.calculateBonus(999999));
+        System.out.println(kontroll.calculateBonus(1000005));
+        System.out.println();
+        System.out.println(kontroll.calculateConsumption(7000, 8000));
+        System.out.println(kontroll.calculateConsumption(8000, 7000));
+        System.out.println();
+        kontroll.printNumbers(6);
+        System.out.println();
+        kontroll.printNumbersBetween(5, 9);
+        System.out.println();
+        kontroll.printNumbersBetweenAnyDirection(1, 4);
+        System.out.println();
+        kontroll.printNumbersBetweenAnyDirection(9, 5);
+        System.out.println();
+        kontroll.printOddNumbers(11);
+        System.out.println();
+        kontroll.printOddNumbers(10);
     }
 }
