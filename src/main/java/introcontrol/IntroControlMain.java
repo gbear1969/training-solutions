5afa30d165335f40c5385ae0475a8c73e0f386d8
gbear1969 <geber.andras@freemail.hu>
2020-11-22 13:00:20 +0100

    introcontrol

---
+++ src/main/java/introcontrol/IntroControlMain.java
@@ -0,0 +1,9 @@
+package introcontrol;
+
+public class IntroControlMain {
+    public static void main(String[] args) {
+        IntroControl szam = new IntroControl();
+        System.out.println(szam.subtractTenIfGreaterThanTen(10));
+        System.out.println(szam.subtractTenIfGreaterThanTen(11));
+    }
+}
