dfa7f82f9a89c21a354018c8fedcdda5a1684e9a
ageber <geber.andras.69@gmail.com>
2020-12-17 13:32:44 +0100

    week08d03

--- src/main/java/week08d03/StringLists.java
+++ src/main/java/week08d03/StringLists.java
@@ -1,22 +1,23 @@
 package week08d03;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StringLists {
-    List<String> union = new ArrayList<>();
+    private List<String> union = new ArrayList<>();
     public List<String> stringListsUnion(List<String> first, List<String> second) {
         for (int i = 0; i < first.size(); i++) {
-            if (!union.contains(first[i])) {
-                union.add(first[i]);
+            if (!union.contains(first.get(i))) {
+                union.add(first.get(i));
             }
         }
         for (int i = 0; i < second.size(); i++) {
-            if (!union.contains(second[i])) {
-                union.add(second[i]);
+            if (!union.contains(second.get(i))) {
+                union.add(second.get(i));
             }
         }
-
+        Collections.sort(union);
         return union;
     }
 
--- src/test/java/week08d03/StringListsTest.java
+++ src/test/java/week08d03/StringListsTest.java
@@ -1,14 +1,21 @@
 package week08d03;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringListsTest {
+
+    @Test
+    public void unionTest() {
         StringLists sl = new StringLists();
         List<String> first = new ArrayList<>(Arrays.asList("b", "c", "a"));
         List<String> second = new ArrayList<>(Arrays.asList("a", "d"));
-        System.out.println(sl.stringListsUnion(first, second));
+
+        assertEquals(Arrays.asList("a", "b", "c", "d"), sl.stringListsUnion(first, second));
     }
 }
