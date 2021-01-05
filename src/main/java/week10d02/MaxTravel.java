c5c91c9832952dfbffbd19c34b284d14aae7b5fd
gbear1969 <geber.andras@freemail.hu>
2021-01-05 13:22:50 +0100

    week10d02

---
+++ src/main/java/week10d02/MaxTravel.java
@@ -0,0 +1,33 @@
+package week10d02;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MaxTravel {
+
+    public int getMaxIndex(List<Integer> stops) {
+        List<Integer> s = new ArrayList<>(stops);
+        Collections.sort(s);
+        int stop = 0;
+        int count = 1;
+        int max = 0;
+        for (int i = 1; i < s.size(); i++) {
+            if (s.get(i - 1) == s.get(i)) {
+                count++;
+            } else {
+                if (count > max) {
+                    max = count;
+                    stop = s.get(i - 1);
+                    count = 1;
+
+                }
+            }
+        }
+        return stop;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MaxTravel().getMaxIndex(List.of(12,12,0,3,4,4)));
+    }
+}
