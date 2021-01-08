51b6245bdaf4d2af3c9a77f10fad85a051367b89
ageber <geber.andras.69@gmail.com>
2021-01-08 16:04:10 +0100

    week10d05

--- src/main/java/week10d05/Calculator.java
+++ src/main/java/week10d05/Calculator.java
@@ -2,32 +2,30 @@ package week10d05;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Calculator {
 
-    public int findMinMaxSum(int[] arr) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int a: arr) {
-            numbers.add(a);
+    public void findMinMaxSum(int[] arr) {
+        if (arr == null || arr.length <  4) {
+            throw new IllegalArgumentException();
         }
-        Collections.sort(numbers);
+        Arrays.sort(arr);
         int sum = 0;
-        if (numbers.size() >  3) {
             for (int i = 0; i < 4; i++) {
-                sum += numbers.get(i);
+                sum += arr[i];
             }
-            return sum;
-        }
-        throw new IllegalArgumentException("Legalább 4 elemű tömböt várok.");
+            System.out.println(sum);;
     }
 
     public static void main(String[] args) {
         Calculator c = new Calculator();
-        int[] t = new int[5];
-        for (int i = 0; i < 5; i++)
+        int l = 10000000;
+        int[] t = new int[l];
+        for (int i = l-1; i > -1; i--)
             t[i] = (i + 1) * 2 - 1;
-        System.out.println(c.findMinMaxSum(t));
+        c.findMinMaxSum(t);
     }
 }
