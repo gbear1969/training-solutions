0894ef3f99a3b8aa5b6164b98f6d3be085c32ada
gbear1969 <geber.andras@freemail.hu>
2021-01-23 22:54:15 +0100

    objectclass

---
+++ src/main/java/objectclass/SimpleBag.java
@@ -0,0 +1,46 @@
+package objectclass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleBag {
+    private List<Object> items = new ArrayList<>();
+    private int cursor = -1;
+
+    public SimpleBag() {
+        beforeFirst();
+    }
+
+    public void putItem(Object item) {
+        items.add(item);
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void beforeFirst() {
+        cursor = -1;
+    }
+
+    public boolean hasNext() {
+        return cursor + 1 < items.size();
+    }
+
+    public Object next() {
+        cursor++;
+        return items.get(cursor);
+    }
+
+    public boolean contains(Object item) {
+        return items.contains(item);
+    }
+
+    public int getCursor() {
+        return cursor;
+    }
+}
