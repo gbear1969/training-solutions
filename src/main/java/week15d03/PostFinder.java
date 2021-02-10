22edb454123438a218ca20eecdeb6d469212e1fd
ageber <geber.andras.69@gmail.com>
2021-02-10 13:11:02 +0100

    week15d03

---
+++ src/main/java/week15d03/PostFinder.java
@@ -0,0 +1,23 @@
+package week15d03;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostFinder {
+    private List<Post> posts = new ArrayList<>();
+
+    public PostFinder(List<Post> posts) {
+        this.posts.addAll(posts);
+    }
+
+    public List<Post> findPostsFor(String user) {
+        List<Post> selectedPosts = new ArrayList<>();
+        for (Post p: posts) {
+            if (p.getOwner().equals(user) && !p.getContent().isEmpty()
+                    && !p.getTitle().isEmpty() && p.getPublishedAt().isBefore(LocalDate.now()));
+            selectedPosts.add(p);
+        }
+        return selectedPosts;
+    }
+}
