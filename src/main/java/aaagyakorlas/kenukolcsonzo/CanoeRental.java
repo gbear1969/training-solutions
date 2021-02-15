15994e613785d4f76342933d0bc6e57d4b97feb8
gbear1969 <geber.andras@freemail.hu>
2021-02-15 16:56:53 +0100

    exam03tests

--- src/main/java/aaagyakorlas/kenukolcsonzo/CanoeRental.java
+++ src/main/java/aaagyakorlas/kenukolcsonzo/CanoeRental.java
@@ -4,7 +4,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class CanoeRental {
-    private String name;
+/*    private String name;
     private CanoeType canoeType;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
@@ -36,6 +36,7 @@ public class CanoeRental {
     }
 
     public double calculateRentalSum() {
-        Duration.between(startTime, endTime).toHours()
-    }
+        Duration.between(startTime, endTime).toHours();
+        return"";
+    }*/
 }
