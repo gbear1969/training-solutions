11f29cd809ad74628e8bd3e6407d8bbb8011c790
gbear1969 <geber.andras@freemail.hu>
2021-02-21 21:37:11 +0100

    ioreadwritebytes.StringToBytes

---
+++ src/main/java/ioreadwritebytes/StringToBytes.java
@@ -0,0 +1,23 @@
+package ioreadwritebytes;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class StringToBytes {
+
+    public void writeAsBytes(List<String> words, Path path) {
+        try (OutputStream outputStream = new
+                BufferedOutputStream(Files.newOutputStream(path))) {
+            for (String word: words) {
+                if (!word.startsWith("_")) outputStream.write(word.getBytes());
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot write file", ioe);
+        }
+
+    }
+}
